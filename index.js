8d57666f51c9e2b5b58dc77c15cb12a66b15751a
David <davidpinerospinto@gmail.com>
2020-10-15 19:22:27 -0500

    agregando archivo css

---
+++ index.js
@@ -0,0 +1,13 @@
+function imprimirvariable(variable)
+
+{console.log(nombre)
+
+    imprimirvariable(David)
+    imprimirvariable(Oscar)
+    
+    var datosdavid = {
+        nombre= "oscar",
+        Telefono= "333",
+        edad= 26,
+        masculino= true
+    }
